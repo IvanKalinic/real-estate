c97a29ec1c562527170f2d7339c7b01c60940568
Ivan Kalinić <ik002312821@fer.hr>
2021-08-11 14:55:41 +0200

    Begin hero section

---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from "../images/house-1.jpg";
+import ImageTwo from "../images/house-2.jpg";
+import ImageThree from "../images/house-3.jpg";
+import ImageFour from "../images/house-4.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa Omiš,Hrvatska",
+    price: "4,000.000 HRK",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in Makarska,Hrvatska",
+    price: "2,000.000 HRK",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa Hvar,Hrvatska",
+    price: "6,000.000 HRK",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "Studio Apartment Primošten,Hrvatska",
+    price: "2,500.000 HRK",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
