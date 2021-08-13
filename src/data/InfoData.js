156b363b18308a77fa417802e78c6105139ec946
Ivan Kalinić <ik002312821@fer.hr>
2021-08-13 12:14:04 +0200

    Extended first page

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from "../images/interior-1.jpg";
+import ImageTwo from "../images/house-5.jpg";
+
+export const InfoData = {
+    heading: "Explore out beautiful homes",
+    paragraphOne :"Lorem ipsum dolor,sit amet consectetur adipisicing elit.",
+    paragraphTwo:"Lorem ipsum dolor sit ansajsdakfsaldfkalsdkflaf",
+    buttonLabel:"View Homes",
+    image:ImageOne,
+    reverse:false,
+    delay:100,
+}
+
+export const InfoDataTwo = {
+    heading: "Modern designs",
+    paragraphOne :"Lorem ipsum dolor,sit amet consectetur adipisicing elit.",
+    paragraphTwo:"Lorem ipsum dolor sit ansajsdakfsaldfkalsdkflaf",
+    buttonLabel:"View Homes",
+    image:ImageTwo,
+    reverse:false,
+    delay:300,
+}
